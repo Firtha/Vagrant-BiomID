4d82da26682ca738828fafd9c437e65317804717
Svensson Jeremy <svensson.jeremy94@gmail.com>
2020-07-07 18:19:59 +0200

    Added MongoDB install/config in provisionning.sh

--- provisionning.sh
+++ provisionning.sh
@@ -13,11 +13,21 @@ sudo apt-get install git-core -y
 wget -qO- https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 
+# MongoDB (https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo chown -R mongodb:mongodb /var/lib/mongodb
+sudo chown mongodb:mongodb /tmp/mongodb-27017.sock
+sudo systemctl enable mongod
+
 # Truffle
 sudo npm install -g --unsafe-perm truffle
 
 # Move scripts to inside the VM
 cp /vagrant/importProject.sh /home/vagrant/importProject.sh
+cp /vagrant/exportNodejs.sh /home/vagrant/exportNodejs.sh
 
 # Manual commands :
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
